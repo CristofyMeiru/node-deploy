374dcebd9c1f509384fc2e19b8dac0d325982275
CristofyMeiru <meiru.cristofy05@gmail.com>
2025-04-15 10:15:10 -0300

    Pagina de formulario

---
+++ src/routes/feira-profissao.ts
@@ -0,0 +1,8 @@
+import { Request, Response, Router } from 'express'
+import path from 'path'
+
+const feiraRoutes = Router()
+
+
+
+export default feiraRoutes
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express, { Application, Request, Response } from 'express'
 import adminRoutes from './routes/admin.routes'
+import feiraRoutes from './routes/feira-profissao'
+import path from 'path'
 
 const app: Application = express()
 const port = process.env.PORT ? Number(process.env.PORT) : 3333
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json())
 
 app.use('/admin', adminRoutes)
-
+app.get("/feira", (req: Request, res: Response) => {
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.listen(port, ()=> {
     console.log("Server is running...")
 })
